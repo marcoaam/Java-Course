2c26aea8f9aac97a0459f65d49302c8d48ba28e1
Marco Araujo <marcopersonal@outlook.com>
2014-08-17 19:52:41 +0100

    Update maxmin.java

--- maxmin.java
+++ maxmin.java
@@ -3,9 +3,9 @@ public class MaxMinPrinter
 {
   public static void main(String[] args)
   {
-  Scanner in = new Scanner(System.in);
-  int max = Integer.MIN_VALUE;
-  int min = Integer.MAX_VALUE;
+    Scanner in = new Scanner(System.in);
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
 
      System.out.print("Enter an integer: ");
      while (in.hasNextInt())
